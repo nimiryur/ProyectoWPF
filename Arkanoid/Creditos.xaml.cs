99d1a91e22e5fe517b24ac0121e89aecda00447d
nimiryur <nimiryur@gmail.com>
2022-01-28 13:03:39 +0100

    act 28

--- Arkanoid/Creditos.xaml.cs
+++ Arkanoid/Creditos.xaml.cs
@@ -10,30 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Arkanoid
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Lógica de interacción para Creditos.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Creditos : Window
     {
-        public MainWindow()
+        public Creditos()
         {
             InitializeComponent();
-            
         }
-        //private void passwordChanged(object sender, RoutedEventArgs e)
-        //{
-        //    if (passwordBox.Password.Length == 0)
-        //        passwordBox.Password = "Hola que ase";
-
-        //}
-
-        //GetClasificacion
-
-
     }
 }
